f39a962939b45fbdbb8573dd0e511889606e4e06
SHIMOMURA Sho <graueule@gmail.com>
2018-02-06 22:03:02 +0900

    share .Xauthority

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm -v $(pwd):/home/ubuntu -w=/home/ubuntu -e DISPLAY=$(ifconfig en0 | grep "inet " | awk '{print $2}'):0 ubuntu16.04_opencv3.4.0 $*
+docker run -it --rm -v $(pwd):/home/ubuntu -v $HOME/.Xauthority:/root/.Xauthority:rw -w=/home/ubuntu -e DISPLAY=$(ifconfig en0 | grep "inet " | awk '{print $2}'):0 ubuntu16.04_opencv3.4.0 $*
